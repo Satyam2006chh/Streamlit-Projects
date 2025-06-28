a90412ede1f8772f884d383357751f4e3f7508d5
Satyam2006chh <satyam3396.beai24@chitkara.edu.in>
2025-06-28 21:29:54 +0530

    Loaded the env var and done with the ai function

--- SmartAnalyst/SmartCSV.py
+++ SmartAnalyst/SmartCSV.py
@@ -5,3 +5,26 @@ import requests
 import plotly.express as px
 from dotenv import load_dotenv
 
+load_dotenv()
+TOGETHER_API_KEY = os.getenv("Your_api_key")
+
+
+def ask_ai(prompt): 
+    url = "https://api.together.xyz/v1/chat/completions"  
+    headers = {
+        "Authorization": f"Bearer {TOGETHER_API_KEY}","Content-Type": "application/json" 
+    }
+    data = {
+        "model": "mistralai/Mixtral-8x7B-Instruct-v0.1",  
+        "messages": [{"role": "user", "content": prompt}], 
+        "temperature": 0.7,  
+        "max_tokens": 800  
+    }
+    response = requests.post(url, headers=headers, json=data)  
+    if response.status_code == 200:
+        return response.json()['choices'][0]['message']['content']  # Extract AI reply
+    else:
+        return f"❌ API Error: {response.text}"  
+    
+
+
